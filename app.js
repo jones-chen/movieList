882e7b770f766860c0fd030374a3ebe938351fae
jones-chen <tonnyqoo@gmail.com>
2023-12-11 14:47:07 +0800

    Add Search Function

--- app.js
+++ app.js
@@ -10,12 +10,31 @@ const movieList = require('./movies.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars') //不用再填寫副檔名
-app.use(express.static('public'))
+app.use(express.static('public'))  //先走 public 這個資料夾，看有沒有檔案
 
 // routes setting
+// index
 app.get('/', (req, res) => {
     // past the movie data into 'index' partial template
-    res.render('index', { movie: movieList.results });
+    res.render('index', { movie: movieList.results});
+})
+
+//搜尋電影
+app.get('/search', (req, res) => {
+    //取得網址的 search=?
+    keyword = req.query.keyword 
+    const newMovieList = movieList.results.filter(movie => {
+        return (movie.title.toLowerCase().includes(keyword.toLowerCase()))
+    })
+    res.render('index', { movie: newMovieList, keyword: keyword});
+})
+
+// movie Info
+app.get('/movies/:movie_id', (req, res) => {
+    movieID = req.params.movie_id
+    movieOne = movieList.results.filter(item => Number(item.id) === Number(movieID))
+    // movieOne = movieList.results[Number(movieID)-1]
+    res.render('show', { movie: movieOne[0] });
 })
 
 // start and listen on the Express server
